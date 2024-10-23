c98f86ddfbebb62712723e2be025abab558641b4
Pablo Ovelleiro Corral <mail@pablo.tools>
2024-10-23 09:14:45 +0200

    Fix fzf-lua config

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,18 +1,27 @@
 return {
 	{
-		dir = pluginpaths  .. "/zk-nvim",
-		name = "zk-nvim",
-		dependencies = {{ dir = pluginpaths .. "/nvim-web-devicons" }},
+		dir = pluginpaths .. "/fzf-lua",
+		name = "fzf-lua",
+		dependencies = { { dir = pluginpaths .. "/nvim-web-devicons" } },
 		config = function()
 
+		print(pluginpaths)
 			require("nvim-web-devicons").setup({
 				-- globally enable default icons (default to false)
 				-- will get overriden by `get_icons` option
-				default = true;
-
+				default = true,
 			})
 
-			require("fzf-lua").setup({})
+			require("fzf-lua").setup({
+				grep = {
+					actions = {
+						["alt-q"] = {
+							fn = require("fzf-lua").actions.file_edit_or_qf,
+							prefix = "select-all+",
+						},
+					},
+				},
+			})
 		end,
 	},
 }
