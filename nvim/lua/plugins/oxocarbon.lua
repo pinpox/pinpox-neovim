6b57ff173acb998289781ddb1d7b74b9e71c80b2
pinpox <git@pablo.tools>
2025-11-14 12:20:44 +0100

    Make nix code less pink

--- nvim/lua/plugins/oxocarbon.lua
+++ nvim/lua/plugins/oxocarbon.lua
@@ -5,6 +5,9 @@ return {
 		config = function()
 			vim.cmd("colorscheme wildcharm")
 
+			-- Make Nix code less pink
+			vim.api.nvim_set_hl(0, '@variable.member', { link = 'Normal' })
+
 			-- Set pure black background
 			-- vim.api.nvim_set_hl(0, "Normal", { bg = "Black" })
 			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "Black" })
