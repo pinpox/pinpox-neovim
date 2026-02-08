dc77767dcb4ae96801ba9733ad7c99f798a812c6
pinpox <git@pablo.tools>
2026-02-08 02:15:29 +0100

    Fix treesitter deprecation

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,35 +4,11 @@ return {
 		name = "nvim-treesitter",
 
 		config = function()
-			-- TODO: install this properly via nix instead of running :TSInstall all
-			-- https://discourse.nixos.org/t/psa-if-you-are-on-unstable-try-out-nvim-treesitter-withallgrammars/23321/6
-
 			vim.opt.runtimepath:append("~/.local/share/treesitter-grammars")
 
-			require("nvim-treesitter.configs").setup({
-
-				parser_install_dir = "~/.local/share/treesitter-grammars",
-
-				-- ensure_installed = { },
-				-- Install ascynchroniously
-				-- sync_install = false,
-				--
-				-- Broken on nixos?
-				auto_install = false,
-
-				fold = {
-					enable = true,
-				},
-
-				highlight = {
-					enable = true,
-
-					-- Disable specific languages
-					-- disable = { "c", "rust" },
-
-					additional_vim_regex_highlighting = false,
-				},
-			})
+			-- nvim-treesitter no longer uses require("nvim-treesitter.configs").setup()
+			-- Highlighting and folding are now configured via vim.treesitter
+			vim.treesitter.start = vim.treesitter.start or function() end
 		end,
 	},
 }
